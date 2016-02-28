b91d8cee34867cff60a38d412fbbea9387acdac2
Edgar To <edjopato@gmail.com>
2016-02-28 22:49:31 +0100

    removeConfig() und getAllConfigs() hinzugefügt

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # telegramBotChatConfigHandler
 
 ### new ChatConfigHandler(folder, [defaultConfigObject])
-Safe all Files to the given Folder. If a config is loaded without an existing file the defaultConfigObject will be returned instead.
+Safe all Files to the given `folder`. If a `config` is loaded without an existing file the `defaultConfigObject` will be returned instead.
+
+### chatConfigHandler.getAllConfigs()
+Load all `config`s.
 
 ### chatConfigHandler.loadConfig(chat)
-Config of the Chat will be loaded from the filesystem. If the file does not exist the defaultConfigObject will be returned.
+`config` of the `chat` will be loaded from the `folder`. If the file does not exist the `defaultConfigObject` will be returned.
 
 ### chatConfigHandler.saveConfig(chat, config)
-A config object will be stored in the file of the chat.
+A `config` object will be stored in the file of the `chat`.
+
+### chatConfigHandler.removeConfig(chat)
+Remove the `config` of `chat` from the `folder`.
--- index.js
+++ index.js
@@ -11,6 +11,12 @@ var ChatConfigHandler = function (folder, defaultConfig) {
   try { fs.mkdirSync(folder, '0755'); } catch (e) { }
 };
 
+ChatConfigHandler.prototype._readFileContent = function (file) {
+  var fileContentString = fs.readFileSync(file, 'utf8')
+  var fileContent = JSON.parse(fileContentString);
+  return fileContent;
+};
+
 ChatConfigHandler.prototype._filenameFromChat = function (chat) {
   var id = chat.id;
   if (id < 0) {
@@ -19,11 +25,14 @@ ChatConfigHandler.prototype._filenameFromChat = function (chat) {
   return this.folder + '/' + id + '.json';
 };
 
+ChatConfigHandler.prototype.getAllConfigs = function () {
+  var files = fs.readdirSync(this.folder);
+  return files.map(f => this._readFileContent(this.folder + '/' + f));
+};
+
 ChatConfigHandler.prototype.loadConfig = function (chat) {
   try {
-    var fileContentString = fs.readFileSync(this._filenameFromChat(chat), 'utf8')
-    var fileContent = JSON.parse(fileContentString);
-    return fileContent.config;
+    return this._readFileContent(this._filenameFromChat(chat)).config;
   } catch (e) {
     return clone(this.defaultConfig);
   }
@@ -33,6 +42,11 @@ ChatConfigHandler.prototype.saveConfig = function (chat, config) {
   var obj = { chat: chat, config: config };
 
   fs.writeFileSync(this._filenameFromChat(chat), JSON.stringify(obj), 'utf8');
+};
+
+ChatConfigHandler.prototype.removeConfig = function (chat) {
+  var filename = this._filenameFromChat(chat);
+  fs.unlinkSync(filename);
 };
 
 module.exports = ChatConfigHandler;
